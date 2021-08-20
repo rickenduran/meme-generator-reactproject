7172d7b7d7dcb09f1fb5179d91ebc1193ba9feff
Rick Duran <63610048+rickenduran@users.noreply.github.com>
2021-08-20 09:04:53 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header.js"
+import MemeGenerator from "./components/MemeGenerator.js"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
+}
+
+export default App
